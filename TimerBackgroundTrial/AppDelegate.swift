23f3a6fd031ee14dd34bb4292f24bbd298e5aa1d
Vaibhav Bangde <Vaibhav_Bangde@Trimble.com>
2017-04-17 17:45:14 +0530

    First Commit.

---
+++ TimerBackgroundTrial/AppDelegate.swift
@@ -0,0 +1,71 @@
+//
+//  AppDelegate.swift
+//  TimerBackgroundTrial
+//
+//  Created by Swapnil Luktuke on 4/13/17.
+//  Copyright © 2017 Swapnil Luktuke. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    let backgroundTaskName = "backgroundTaskName"
+    var backgroundTaskId : UIBackgroundTaskIdentifier?
+    var counter : Int = 0
+    let timerIntervalSeconds : Int = 10
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        if #available(iOS 10.0, *) {
+            Timer.scheduledTimer(withTimeInterval: Double(timerIntervalSeconds) as Double, repeats: true) { (timer) in
+                self.counter += 1
+                print("Count - \(self.counter) | Time - \((self.counter * self.timerIntervalSeconds/60)):\((self.counter * self.timerIntervalSeconds)%60))")
+            }
+        } else {
+            // Fallback on earlier versions
+        }
+        
+        return true
+    }
+
+    func applicationWillResignActive(_ application: UIApplication) {
+        print("applicationWillResignActive")
+        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+        backgroundTaskId = UIApplication.shared.beginBackgroundTask(withName: backgroundTaskName) {
+            print("backgroundTaskName")
+        }
+        
+    }
+    
+    func applicationDidEnterBackground(_ application: UIApplication) {
+        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+        print("applicationDidEnterBackground")
+    }
+    
+    func applicationWillEnterForeground(_ application: UIApplication) {
+        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+        print("applicationWillEnterForeground")
+    }
+    
+    func applicationDidBecomeActive(_ application: UIApplication) {
+        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        print("applicationDidBecomeActive")
+        if let taskId = backgroundTaskId {
+            UIApplication.shared.endBackgroundTask(taskId)
+        }
+        
+    }
+    
+    func applicationWillTerminate(_ application: UIApplication) {
+        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+        print("applicationWillTerminate")
+    }
+
+}
+
