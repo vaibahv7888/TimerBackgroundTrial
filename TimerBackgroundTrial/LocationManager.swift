4a7f04b118263f43d385c1e983dcb1c4276181cc
Swapnil Luktuke <Swapnil_Luktuke@Trimble.com>
2017-05-04 12:56:10 +0530

    Code refactoring, moved error and location handling to separate methods.

--- TimerBackgroundTrial/LocationManager.swift
+++ TimerBackgroundTrial/LocationManager.swift
@@ -18,6 +18,9 @@ class LocationManager : NSObject, CLLocationManagerDelegate {
     private var _locationManager : CLLocationManager?
     private var _deferedLocUpdateAllowed = false
 
+    private var _lastLocationupdateTimeStamp : TimeInterval = 0
+    
+    
     private func getLocationManager() -> CLLocationManager {
         if nil == _locationManager {
             _locationManager = CLLocationManager()
@@ -48,39 +51,64 @@ class LocationManager : NSObject, CLLocationManagerDelegate {
         getLocationManager().startMonitoringSignificantLocationChanges()
     }
 
+    func stopMonitoringSignificantLocationChanges() {
+        NSLog("\(#function)")
+        getLocationManager().stopMonitoringSignificantLocationChanges()
+    }
     
     
     // MARK: Location manager delegate
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]){
         NSLog("\(#function)")
-        Logger.println(s: "\(getCurrentTime()) | didUpdateLocations")
-        
-        for location in locations {
-            Logger.println(s: "\t\t\t | Loc(lat:long) -  \(location.coordinate.latitude) : \(location.coordinate.latitude) |  Accuracy : \(location.horizontalAccuracy) | Timestamp : \(location.timestamp) ")
-        }
-        
+        handleLocationsUpdate(locations: locations)
         if !_deferedLocUpdateAllowed {
             _locationManager?.allowDeferredLocationUpdates(untilTraveled: CLLocationDistanceMax, timeout: 300)
             _deferedLocUpdateAllowed = true
         }
     }
     
+    public func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        NSLog("\(#function)")
+        handleLocationError(error: error)
+    }
+
     public func locationManagerDidPauseLocationUpdates(_ manager: CLLocationManager) {
         NSLog("\(#function)")
-        Logger.println(s: "\(getCurrentTime()) | locationManagerDidPauseLocationUpdates")
+        Logger.println(s: "\(getCurrentTime()) | \(#function)")
     }
     
     
     public func locationManagerDidResumeLocationUpdates(_ manager: CLLocationManager) {
         NSLog("\(#function)")
-        Logger.println(s: "\(getCurrentTime()) | locationManagerDidResumeLocationUpdates")
+        Logger.println(s: "\(getCurrentTime()) | \(#function)")
     }
     
     public func locationManager(_ manager: CLLocationManager, didFinishDeferredUpdatesWithError error: Error?) {
         if nil != error {
             NSLog("\(#function) error : \(error!.localizedDescription)")
-            Logger.println(s: "\(getCurrentTime()) | didFinishDeferredUpdatesWithError : \(error!.localizedDescription)")
+            handleLocationError(error: error!)
         }
         manager.startUpdatingLocation()
     }
+    
+    
+    
+    // MARK: Logging
+    public func handleLocationError(error: Error) {
+        Logger.println(s: "\(getCurrentTime()) | \(#function) : \(error.localizedDescription)")
+    }
+    
+    public func handleLocationsUpdate(locations: [CLLocation]) {
+        Logger.println(s: "\(getCurrentTime()) | \(#function) | _lastLocTmStmp : \(_lastLocationupdateTimeStamp)")
+        for location in locations {
+            if (location.timestamp.timeIntervalSince1970 - _lastLocationupdateTimeStamp) > 30 {
+                _lastLocationupdateTimeStamp = location.timestamp.timeIntervalSince1970
+                NSLog("\(#function) | _lastLocTmStmp : \(_lastLocationupdateTimeStamp) | _locTmStmp : \(location.timestamp.timeIntervalSince1970)")
+                Logger.println(s: "\t\t\t | Loc(lat:long) -  \(location.coordinate.latitude) : \(location.coordinate.latitude) |  Accuracy : \(location.horizontalAccuracy) | Timestamp : \(location.timestamp) ")
+            }
+        }
+    }
+    
+    
+    
 }
